e5275a1fb6fe8972e08ee08e4b42726709b28ecc
RossouwBinedellAliens <ross@cowboyaliens.com>
2016-06-21 11:30:12 +0200

    Fifth comment

--- app/src/main/java/maintest/calcapp/MainActivity.java
+++ app/src/main/java/maintest/calcapp/MainActivity.java
@@ -3,21 +3,28 @@ package maintest.calcapp;
 import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private RadioGroup rbg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.activity_main_radioButton_add).setSelected(true);
+        Log.d("OnClick", "onStart: " );
         TextView tw = (TextView) findViewById(R.id.textView_Output);
         tw.setText("");
+        rbg = (RadioGroup) findViewById(R.id.activity_main_radioGroup);
+        rbg.check(R.id.activity_main_radioButton_add);
 
 
         Button btnSubmit = (Button) findViewById(R.id.commitButton);
@@ -28,11 +35,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         if (v.getId() == R.id.commitButton) {
             int selection = getSelection();
+            Log.d("OnClick", "onClick: Selection: " + selection);
             EditText num1 = (EditText) findViewById(R.id.activity_main_firstNum);
             EditText num2 = (EditText) findViewById(R.id.activity_main_secondNum);
             int total = 0;
             if (num1.getText().toString().isEmpty() || num2.getText().toString().isEmpty()) {
-                Toast.makeText(MainActivity.this, "Please enter a valid number!", Toast.LENGTH_SHORT);
+                Toast.makeText(MainActivity.this, "Please enter a valid number!", Toast.LENGTH_SHORT).show();
             } else {
                 switch (selection) {
                     case 1:
@@ -51,25 +59,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         total = -1;
                 }
                 TextView outputText = (TextView) findViewById(R.id.textView_Output);
-                outputText.setText(total+"");
+               //try {
+                   outputText.setText(total+"");
+               //} catch ( NullPointerException n){
+                 //  System.out.println(n.getMessage());
+               //}
 
             }
         }
     }
 
     private int getSelection() {
-        if (findViewById(R.id.activity_main_radioButton_add).isSelected()) {
+        if (rbg.getCheckedRadioButtonId()==R.id.activity_main_radioButton_add) {
             return 1;
-        } else if (findViewById(R.id.activity_main_radioButton_subtract).isSelected()) {
+        } else if (rbg.getCheckedRadioButtonId()==R.id.activity_main_radioButton_subtract) {
             return 2;
-        } else if (findViewById(R.id.activity_main_radioButton_multiply).isSelected()) {
+        } else if (rbg.getCheckedRadioButtonId()==R.id.activity_main_radioButton_multiply) {
             return 3;
-        } else if (findViewById(R.id.activity_main_radioButton_divide).isSelected()) {
+        } else if (rbg.getCheckedRadioButtonId()==R.id.activity_main_radioButton_divide) {
             return 4;
         } else {
             return 0;
         }
-
 
     }
 }
